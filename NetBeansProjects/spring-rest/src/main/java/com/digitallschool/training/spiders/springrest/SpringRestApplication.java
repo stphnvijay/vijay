5acf8e86124a30a86bdaca989cbfd204fca613d5
stphnvj07 <stphnvijay@gmail.com>
2020-02-08 00:29:53 +0530

    "raviProj"

---
+++ NetBeansProjects/spring-rest/src/main/java/com/digitallschool/training/spiders/springrest/SpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.digitallschool.training.spiders.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApplication.class, args);
+	}
+
+}
